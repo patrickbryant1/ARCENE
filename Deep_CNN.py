69e911fae890c0f26d52cebeeec15e0067b30f68
patrickbryant1 <patrick.bryant@live.com>
2018-02-06 10:34:16 +0000

    Create Deep_CNN.py
    Description added.

---
+++ Deep_CNN.py
@@ -0,0 +1,5 @@
+'''
+This is a deep CNN that trains and evaluates on the ARCENE dataset which main objective is to
+distinguish cancer versus normal patterns from massspectrometric data. The Python programming language
+in combination with TensorFlow, which uses a highly efficient C++ backend for computations.
+'''
